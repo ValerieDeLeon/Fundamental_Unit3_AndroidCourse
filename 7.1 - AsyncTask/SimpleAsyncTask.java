fb0f1f13394528278bc5a85fe48aed9e0f763bb6
Valerie <valeriedeleon4521@gmail.com>
2020-03-12 08:53:26 -0700

    Update SimpleAsyncTask.java

--- 7.1 - AsyncTask/SimpleAsyncTask.java
+++ 7.1 - AsyncTask/SimpleAsyncTask.java
@@ -1,17 +1,28 @@
 package com.example.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask <Void, Void, String>
+public class SimpleAsyncTask extends AsyncTask <Void, Integer, String>
 {
     private WeakReference<TextView> mTextView;
-
-    SimpleAsyncTask(TextView tv) {
+    private ProgressBar mProgressBar;
+    private Handler mHandler = new Handler();
+    SimpleAsyncTask(TextView tv, ProgressBar pb)
+    {
         mTextView = new WeakReference<>(tv);
+        mProgressBar = pb;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -27,6 +38,7 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String>
 
         // Sleep for the random amount of time
         try {
+            publishProgress(s);
             Thread.sleep(s);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -36,7 +48,16 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String>
         return "Awake at last after sleeping for " + s + " milliseconds!";
     }
 
-    protected void onPostExecute(String result) {
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+
+        mProgressBar.setProgress(values[0]);
+    }
+
+    protected void onPostExecute(String result)
+    {
         mTextView.get().setText(result);
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 }
