ea8c40d8eb336f6a5e9c52aa25931b3ceb87c077
Valerie <valeriedeleon4521@gmail.com>
2020-03-13 08:41:42 -0700

    Update SimpleAsyncTask.java

--- 7.1 - AsyncTask/SimpleAsyncTask.java
+++ 7.1 - AsyncTask/SimpleAsyncTask.java
@@ -1,8 +1,6 @@
 package com.example.simpleasynctask;
 
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,17 +10,13 @@ import java.util.Random;
 public class SimpleAsyncTask extends AsyncTask <Void, Integer, String>
 {
     private WeakReference<TextView> mTextView;
-    private ProgressBar mProgressBar;
-    private Handler mHandler = new Handler();
+    private WeakReference<ProgressBar> mProgressBar;
+    private static final int CHUNK_SIZE = 5;
+
     SimpleAsyncTask(TextView tv, ProgressBar pb)
     {
         mTextView = new WeakReference<>(tv);
-        mProgressBar = pb;
-    }
-
-    @Override
-    protected void onPreExecute() {
-        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar = new WeakReference<>(pb);
     }
 
     @Override
@@ -31,33 +25,33 @@ public class SimpleAsyncTask extends AsyncTask <Void, Integer, String>
         // Generate a random number between 0 and 10
         Random r = new Random();
         int n = r.nextInt(11);
+        int p = n * 400;
+        int chunkSize = p / CHUNK_SIZE;
 
         // Make the task take long enough that we have
         // time to rotate the phone while it is running
-        int s = n * 200;
 
         // Sleep for the random amount of time
-        try {
-            publishProgress(s);
-            Thread.sleep(s);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < CHUNK_SIZE; i++) {
+            try {
+                Thread.sleep(chunkSize);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            publishProgress(((i + 1) * 100) / CHUNK_SIZE);
         }
 
         // Return a String result
-        return "Awake at last after sleeping for " + s + " milliseconds!";
+        return "Awake at last after sleeping for " + p + " milliseconds!";
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
-        super.onProgressUpdate(values);
-
-        mProgressBar.setProgress(values[0]);
+        mProgressBar.get().setProgress(values[0]);
     }
 
     protected void onPostExecute(String result)
     {
         mTextView.get().setText(result);
-        mProgressBar.setVisibility(View.VISIBLE);
     }
 }
