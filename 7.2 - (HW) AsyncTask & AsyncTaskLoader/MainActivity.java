2f3685203796bc10fa9c94b9f0b5dc0dea755689
Valerie <valeriedeleon4521@gmail.com>
2020-06-03 12:00:09 -0700

    Update MainActivity.java

--- 7.2 - (HW) AsyncTask & AsyncTaskLoader/MainActivity.java
+++ 7.2 - (HW) AsyncTask & AsyncTaskLoader/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.url_locator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner urlSpinner = findViewById(R.id.urlSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tags, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        urlSpinner.setAdapter(adapter);
+        urlSpinner.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
